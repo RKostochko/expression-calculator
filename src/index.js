b9de736d4ce4186160154c34eb18829fed1405fa
RKostochko <kostochko.tech@mail.ru>
2021-08-21 21:44:47 +0300

    fead: complete task in 52 min :(

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 function eval() {
-    // Do not use eval!!!
-    return;
+	// Do not use eval!!!
+	return;
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+	// write your solution here
+	expr = expr.replace(/ /g, '');
+	if (expr.indexOf('/0') !== -1)
+		throw new Error('TypeError: Division by zero.');
+
+
+	let brackets = 0;
+	expr.split('').forEach((el) => {
+		el === '(' ? brackets++ : false;
+		el === ')' ? brackets-- : false;
+	});
+	if (brackets !== 0)
+		throw new Error('ExpressionError: Brackets must be paired');
+
+
+	expr = new Function(`return ${expr}`);
+
+	return expr();
 }
 
 module.exports = {
-    expressionCalculator
+	expressionCalculator
 }
